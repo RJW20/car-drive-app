8b3c7ac40b9fd76021f78ac8c273c36f88e8d86b
RJW20 <robertjwatts84@outlook.com>
2024-05-22 13:25:32 +0100

    Implement some vector operations

--- car_drive_app/cartesians/vector.py
+++ car_drive_app/cartesians/vector.py
@@ -1,4 +1,4 @@
-
+from __future__ import annotations
 
 
 class Vector:
@@ -7,3 +7,15 @@ class Vector:
     def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector(self.x - other.x, self.y - other.y)
+    
+    def __mul__(self, other: float) -> Vector:
+        return Vector(self.x * other, self.y * other)
+    
+    def __rmul__(self, other: float) -> Vector:
+        return Vector(self.x * other, self.y * other)
