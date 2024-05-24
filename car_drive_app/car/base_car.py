1650b2881f5a6cf469b3f9567e6d4990ebdc4a5c
RJW20 <robertjwatts84@outlook.com>
2024-05-24 15:07:20 +0100

    Implement acceleration to car

--- car_drive_app/car/base_car.py
+++ car_drive_app/car/base_car.py
@@ -1,69 +1,70 @@
 import math
 
+from car_drive_app.car.rigid_body import RigidBody
+from car_drive_app.car.wheel import Wheel
 from car_drive_app.cartesians import Vector
 from car_drive_app.car.turn import Turn
 
 
-class BaseCar:
+class BaseCar(RigidBody):
     """The underlying class for the Car that drives around the Track."""
 
     LENGTH = 80
     WIDTH = 40
-    MASS = 1
+    MASS = 200
     POWER = 0.5
     DRAG_COEFFICIENT = 0.002
-    ROLLING_COEFFICIENT = 0.06
-    WHEELS_OFFSET = 0.4 * LENGTH
 
     def __init__(self) -> None:
-        self.position: Vector
-        self.velocity: Vector
-        self.orientation: float
-        self.turning_angle: float
-        self.turning_angular_velocity: float
+        # Set up the Wheels
+        length_offset, width_offset = 0.4 * self.LENGTH, 0.5 * self.WIDTH
+        fr_wheel = Wheel(Vector(length_offset,width_offset))
+        fl_wheel = Wheel(Vector(length_offset,-width_offset))
+        br_wheel = Wheel(Vector(-length_offset,width_offset))
+        bl_wheel = Wheel(Vector(-length_offset,-width_offset))
+        self.wheels: list[Wheel] = [fr_wheel, fl_wheel, br_wheel, bl_wheel]
+
+        super().__init__(Vector(80,40), self.MASS + 4 * self.wheels[0].MASS)
 
     def reset(self) -> None:
         """Return the Car to the start point."""
 
-        self.position = Vector(100,400)
-        self.velocity = Vector(0,0)
-        self.orientation = 0
-        self.turning_angle = 0
-        self.turning_angular_velocity = 0
+        super().reset()
+        for wheel in self.wheels:
+            wheel.reset()
 
     @property
     def direction(self) -> Vector:
-        """Return the unit vector in direction self.orientation."""
-        return Vector.unit_from_angle(self.orientation)
+        """Return the unit vector in direction self.angle."""
+        return Vector.unit_from_angle(self.angle)
+    
+    @property
+    def front_wheels(self) -> list[Wheel]:
+        """Return the front Wheels of the Car."""
+        return self.wheels[:2]
+    
+    @property
+    def back_wheels(self) -> list[Wheel]:
+        """Return the back Wheels of the Car."""
+        return self.wheels[2:]
 
     def move(self, turn: Turn, accelerate: bool) -> None:
         """Advance the Car one frame."""
 
-        # Acceleration
-        traction = self.POWER * self.direction if accelerate else Vector(0,0)
-        drag = - self.DRAG_COEFFICIENT * self.velocity.magnitude * self.velocity
-        rolling = - self.ROLLING_COEFFICIENT * self.velocity
-        acceleration = (traction + drag + rolling) / self.MASS
-        self.velocity += acceleration
+        torque_applied = 1 if accelerate else 0
+        for wheel in self.front_wheels:
+            self.add_force(wheel.force_exerted(-self.velocity, 0))
+        for wheel in self.back_wheels:
+            self.add_force(wheel.force_exerted(-self.velocity, torque_applied))
 
-        # Turning
-        self.turning_angle = max(min(self.turning_angle + turn.value, math.pi/3), -math.pi/3)
-        if not self.turning_angle == 0:
-            turning_radius = 2 * self.WHEELS_OFFSET / math.sin(self.turning_angle)
-            self.turning_angular_velocity = self.velocity.magnitude/turning_radius
-            self.velocity.rotate_by(self.turning_angular_velocity) 
-
-        # Adjust the position and orientation
-        self.position += self.velocity + self.WHEELS_OFFSET * (Vector.unit_from_angle(self.orientation + self.turning_angular_velocity) - self.direction)
-        self.orientation += self.turning_angular_velocity
-        self.turning_angle /= 1.1
+        super().update()
 
     @property
     def outline(self) -> list[Vector]:
         """Return 2D points that make up the outline of the Car."""
 
         length_ways = self.direction * (self.LENGTH // 20)
-        width_ways = Vector.unit_from_angle(self.orientation + math.pi/2) * (self.WIDTH // 10)
+        width_ways = Vector.unit_from_angle(self.angle + math.pi/2) * (self.WIDTH // 10)
 
         front_center = self.position + 10 *  length_ways
         front = [front_center + i * width_ways for i in range(-5,6)]
