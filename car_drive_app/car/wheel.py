b3485890eb7468eaf41e26eeac14514c7fb25cdb
RJW20 <robertjwatts84@outlook.com>
2024-05-24 11:35:30 +0100

    Rewrite the frictional force calculation

--- car_drive_app/car/wheel.py
+++ car_drive_app/car/wheel.py
@@ -6,8 +6,10 @@ from car_drive_app.cartesians import Vector, dot
 class Wheel:
 
     RADIUS = 5
-    INERTIA = 1
-    FRICTION_COEFFICIENT = 0.9
+    MASS = 10
+    INERTIA = MASS * RADIUS ** 2 / 2
+    SIDE_FRICTION = 2
+    FORWARD_FRICTION = 1
 
     def __init__(self) -> None:
         self.rotation_speed: float
@@ -27,38 +29,45 @@ class Wheel:
     def turn_angle(self, value: float):
         """Set the Wheels angle relative to the Chassis.
         
-        Also sets the forward and side axes (in the frame of the Car)."""
+        Also sets the forward and side axes (in the frame of the Car).
+        """
 
         self._turn_angle = value
         self.forward_axis = Vector.unit_from_angle(value)
         self.side_axis = Vector.unit_from_angle(value + math.pi/2)
 
-    def friction_magnitude(self, relative_ground_velocity: Vector, axis: Vector, weight: float) -> float:
-        """Return the magnitude of the frictional force that is acting along the given axis."""
+    def side_friction(self, slip_velocity: Vector) -> float:
+        """Return the magnitude of the frictional force that is acting along self.side_axis.
+        
+        Modelled as self.SIDE_FRICTION times the slipping speed in the side direction acting
+        in the opposide direction.
+        """
+        return - self.SIDE_FRICTION * dot(slip_velocity, self.side_axis)
+    
+    def forward_friction(self, slip_velocity: Vector) -> float:
+        """Return the magnitude of the frictional force that is acting along self.forward_axis.
+        
+        Modelled as self.FORWARD_FRICTION times the slipping speed in the forward direction
+        acting in the opposite direction.
+        """
 
-        speed = dot(relative_ground_velocity, axis)
+        return - self.FORWARD_FRICTION * dot(slip_velocity, self.forward_axis)
 
-        if math.isclose(speed, 0, rel_tol=1e-07):
-            return 0
-
-        if speed > 0:
-            return self.FRICTION_COEFFICIENT * weight
-        else:
-            return - self.FRICTION_COEFFICIENT * weight
-
-    def force_exerted(self, ground_velocity: Vector, weight: float) -> Vector:
-        """Return the force the Wheel is exerting on the Chassis given the ground velocity and weight
-        that is distributed over the Wheel."""
+    def force_exerted(self, ground_velocity: Vector) -> Vector:
+        """Return the force the Wheel is exerting on the Chassis given the ground velocity."""
 
         resultant_force = Vector(0,0)
         torque = 0
 
+        # Calculate which way the tyre is slipping
+        tyre_surface_velocity = self.RADIUS * self.rotation_speed * self.forward_axis
+        slip_velocity = tyre_surface_velocity - ground_velocity
+
         # Sideways friction
-        resultant_force += self.friction_magnitude(ground_velocity, self.side_axis, weight) * self.side_axis
+        resultant_force += self.side_friction(slip_velocity) * self.side_axis
 
         # Forwards friction
-        tyre_velocity = - self.RADIUS * self.rotation_speed * self.forward_axis
-        forward_magnitude = self.friction_magnitude(ground_velocity + tyre_velocity, self.forward_axis, weight)
+        forward_magnitude = self.forward_friction(slip_velocity)
         resultant_force += forward_magnitude * self.forward_axis
         torque += forward_magnitude * self.RADIUS
         
