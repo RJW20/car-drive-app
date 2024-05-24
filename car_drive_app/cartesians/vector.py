e558d028fbc5f04522b479673decc889980eb3d1
RJW20 <robertjwatts84@outlook.com>
2024-05-24 16:07:30 +0100

    Remove cached trig functions

--- car_drive_app/cartesians/vector.py
+++ car_drive_app/cartesians/vector.py
@@ -1,16 +1,5 @@
 from __future__ import annotations
-from functools import lru_cache
 import math
-
-
-@lru_cache
-def cached_sine(theta: float) -> float:
-    return math.sin(theta)
-
-
-@lru_cache
-def cached_cosine(theta: float) -> float:
-    return math.cos(theta)
 
 
 class Vector:
@@ -49,16 +38,13 @@ class Vector:
     @classmethod
     def unit_from_angle(cls, angle: float) -> Vector:
         """Return the unit Vector pointing along the given angle."""
-        return cls(cached_cosine(angle), cached_sine(angle))
+        return cls(math.cos(angle), math.sin(angle))
     
     def rotate_by(self, angle: float) -> Vector:
         """Return the Vector that is produced by the clockwise rotation by the given angle."""
 
-        # Put the angle in the range 0-2pi to maximise efficiency in cached trig functions
-        angle = angle % (2 * math.pi)
-
-        x = cached_cosine(angle) * self.x - cached_sine(angle) * self.y
-        y = cached_sine(angle) * self.x + cached_cosine(angle) * self.y
+        x = math.cos(angle) * self.x - math.sin(angle) * self.y
+        y = math.sin(angle) * self.x + math.cos(angle) * self.y
         return Vector(x, y)
 
 
