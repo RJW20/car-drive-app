9dd8754f5ad6c2509faed378b74668cccdca2f04
RJW20 <robertjwatts84@outlook.com>
2024-05-31 13:40:52 +0100

    Contain point generation to be away from the edges

--- car_drive_app/track/generator/random_points.py
+++ car_drive_app/track/generator/random_points.py
@@ -3,16 +3,19 @@ import random
 from car_drive_app.cartesians import Vector
 
 
-def random_points(dimensions: Vector) -> list[Vector]:
+def random_points(dimensions: Vector, dist_from_edges: int = 0) -> list[Vector]:
     """Return a list of random points in the 2D plane.
     
     The number of points will have average density of approximately 1 point 
     per 60x60 square.
+    No point will be closer than dist_from_edges to the outside of the dimensions.
     Raises a value error if the dimensions are too small.
     """
 
     # Calculate the number of points to generate
-    area = dimensions.x * dimensions.y
+    offset = Vector(dist_from_edges, dist_from_edges)
+    available_dimensions = dimensions - 2 * offset
+    area = available_dimensions.x * available_dimensions.y
     min_points = area // 80 ** 2
     if min_points < 9:
         raise ValueError(f'Dimensions {dimensions} is too small to generate a track on.')
@@ -20,6 +23,6 @@ def random_points(dimensions: Vector) -> list[Vector]:
     num_points = random.randint(min_points, max_points)
 
     # Generate the points
-    points = [Vector(random.randint(1,dimensions.x-1), random.randint(1,dimensions.y-1)) for _ in range(num_points)]
+    points = [offset + Vector(random.randint(1,available_dimensions.x-1), random.randint(1,available_dimensions.y-1)) for _ in range(num_points)]
     
     return points
