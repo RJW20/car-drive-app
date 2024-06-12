e539c42ec6e203504d98bfae852bee0944797ec4
RJW20 <robertjwatts84@outlook.com>
2024-06-12 14:20:01 +0100

    Allow both types of track creation

--- scripts/creator.py
+++ scripts/creator.py
@@ -1,14 +1,25 @@
 import sys
 
 from car_drive_app.cartesians import Vector
-from car_drive_app.track.creator.points_and_circles import Creator
+import car_drive_app.track.creator.points_and_circles
+import car_drive_app.track.creator.points_and_splines
 
 
-def creator(args: list=sys.argv) -> None:
+def creator_1(args: list=sys.argv) -> None:
     """Starts and runs new instance of points_and_circles.Creator with dimensions
     and save name specified in args."""
 
     dimensions = Vector(int(args[1]), int(args[2]))
     track_save_name = args[3]
-    cr = Creator(dimensions, track_save_name)
+    cr = car_drive_app.track.creator.points_and_circles.Creator(dimensions, track_save_name)
+    cr.run()
+
+
+def creator_2(args: list=sys.argv) -> None:
+    """Starts and runs new instance of points_and_splines.Creator with dimensions
+    and save name specified in args."""
+
+    dimensions = Vector(int(args[1]), int(args[2]))
+    track_save_name = args[3]
+    cr = car_drive_app.track.creator.points_and_splines.Creator(dimensions, track_save_name)
     cr.run()
