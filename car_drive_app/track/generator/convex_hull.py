139b68d62c6ec89f9c17ffbeb4c0e23f27f2bafa
RJW20 <robertjwatts84@outlook.com>
2024-06-12 16:58:46 +0100

    Reverse the order the convex_hull algorithm returns the points in

--- car_drive_app/track/generator/convex_hull.py
+++ car_drive_app/track/generator/convex_hull.py
@@ -7,19 +7,19 @@ def find_start(points: list[Vector]) -> Vector:
 
 
 def next_most_left(start: Vector, others: list[Vector]) -> Vector:
-    """Return the point in others that is the most left of the start Vector (and hence all others
-    must be to the right).
+    """Return the point in others that is the most right of the start Vector (and hence all others
+    must be to the left).
     
     Sets a pointer at the first point in others, then iterates through points in others and calculates 
     the 2D cross product of the Vectors start->pointer and start->current_point and whenever it is
-    negative switches the pointer to current_point then continues.
+    positive switches the pointer to current_point then continues.
     """
 
     choice = others[0]
     choice_offset = choice - start
     for point in others[1:]:
         point_offset = point - start
-        if cross(choice_offset, point_offset) < 0:
+        if cross(choice_offset, point_offset) > 0:
             choice = point
             choice_offset = choice - start
 
@@ -30,7 +30,7 @@ def convex_hull(points: list[Vector]) -> list[Vector]:
     """Return the convex hull of the given points.
     
     Uses the gift wrapping algorithm.
-    Returns the points in anti-clockwise order (clockwise on the regular cartesian plane).
+    Returns the points in clockwise order (anti-clockwise on the regular cartesian plane).
     """
 
     hull = []
