7a3fd61ccdb39a47b0bbd848b0636a8a1c3a0c78
RJW20 <robertjwatts84@outlook.com>
2024-06-13 13:58:34 +0100

    Set up the generator as determined from the tests

--- car_drive_app/track/generator/generator.py
+++ car_drive_app/track/generator/generator.py
@@ -5,6 +5,8 @@ from car_drive_app.cartesians import Vector
 from car_drive_app.track.generator.random_points import random_points
 from car_drive_app.track.generator.convex_hull import convex_hull
 from car_drive_app.track.generator.displaced_midpoints import displaced_midpoints
+from car_drive_app.track.generator.push_apart import push_apart
+from car_drive_app.track.generator.fix_angles import fix_angles
 from car_drive_app.track.creator.points_and_splines.catmull_rom import catmull_rom
 
 
@@ -14,10 +16,11 @@ def generator(dimensions: Vector, track_save_name: str) -> BaseTrack:
 
     TRACK_WIDTH = 150
 
-    points = random_points(dimensions, 2 * TRACK_WIDTH)
+    points = random_points(dimensions, TRACK_WIDTH)
     c_hull = convex_hull(points)
-    points_and_mps = displaced_midpoints(c_hull, 1, TRACK_WIDTH)
-    # Push apart and fix angles
+    points_and_mps = displaced_midpoints(c_hull, 1, 2 * TRACK_WIDTH)
+    while push_apart(points_and_mps, TRACK_WIDTH) or fix_angles(points_and_mps):
+        pass
     full_curve = catmull_rom(points_and_mps)
     full_curve.rotate(random.randint(0, len(full_curve) - 1))
     track = BaseTrack(dimensions, full_curve, TRACK_WIDTH)
