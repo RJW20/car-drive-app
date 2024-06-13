923c242cd89ecb4fb456c9b9b7e432b3de896090
RJW20 <robertjwatts84@outlook.com>
2024-06-13 13:55:40 +0100

    Reduce random_points density to get a track with fewer clustered areas

--- car_drive_app/track/generator/random_points.py
+++ car_drive_app/track/generator/random_points.py
@@ -3,26 +3,26 @@ import random
 from car_drive_app.cartesians import Vector
 
 
-def random_points(dimensions: Vector, dist_from_edges: int = 0) -> list[Vector]:
+def random_points(dimensions: Vector, offset: int) -> list[Vector]:
     """Return a list of random points in the 2D plane.
     
+    All points will be at offset distance away from the nearest walls.
     The number of points will have average density of approximately 1 point 
-    per 60x60 square.
-    No point will be closer than dist_from_edges to the outside of the dimensions.
+    per 240x240 square.
     Raises a value error if the dimensions are too small.
     """
 
     # Calculate the number of points to generate
-    offset = Vector(dist_from_edges, dist_from_edges)
+    offset = Vector(offset, offset)
     available_dimensions = dimensions - 2 * offset
     area = available_dimensions.x * available_dimensions.y
-    min_points = area // 80 ** 2
+    min_points = area // 320 ** 2
     if min_points < 9:
         raise ValueError(f'Dimensions {dimensions} is too small to generate a track on.')
-    max_points = area // 50 ** 2
+    max_points = area // 200 ** 2
     num_points = random.randint(min_points, max_points)
 
     # Generate the points
-    points = [offset + Vector(random.randint(1,available_dimensions.x-1), random.randint(1,available_dimensions.y-1)) for _ in range(num_points)]
+    points = [offset + Vector(random.randint(0,available_dimensions.x), random.randint(0,available_dimensions.y)) for _ in range(num_points)]
     
     return points
