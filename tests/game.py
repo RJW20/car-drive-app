b3954c503bbfc178cf6ad83e3e96a889b5e40e2e
RJW20 <robertjwatts84@outlook.com>
2024-06-27 12:14:15 +0100

    Make one of the 'tests' use inheritance

--- tests/game.py
+++ tests/game.py
@@ -3,65 +3,11 @@ import math
 
 import pygame
 
-from car_drive_app.track.track import Track
-from car_drive_app.car.car import Car
-from car_drive_app.car import Acceleration
+from car_drive_app.game import Game
 
 
-class Game:
+class GameVisual(Game):
     """Game controller but all parts used in calculations are drawn on the screen."""
-
-    def __init__(self, track_save_name: str) -> None:
-
-        # Load the Track
-        self.track = Track.load(track_save_name)
-
-        # Start the Car
-        self.car = Car()
-        self.track.place_car_at_start(self.car)
-        
-        # Pygame set up
-        self.dimensions = self.track.dimensions
-        pygame.init()
-        self.screen = pygame.display.set_mode((self.dimensions.x, self.dimensions.y))
-        pygame.display.set_caption("Car Drive")
-        self.clock = pygame.time.Clock()
-        try:
-            self.joystick = pygame.joystick.Joystick(0)
-            self.joystick.init()
-        except pygame.error:
-            raise Exception('No Controller found')
-
-    def check_move(self) -> tuple[float, Acceleration]:
-        """Check for new user input and convert to valid move."""
- 
-        # Allow quitting
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                pygame.quit()
-                exit()
-
-        # Check for user input
-        turn_angle = self.joystick.get_axis(0) * math.pi / 4
-
-        accelerate = self.joystick.get_button(0)
-        brake = self.joystick.get_button(1)
-        if accelerate:
-            acceleration = Acceleration.FORWARD
-        elif brake:
-            acceleration = Acceleration.REVERSE
-        else:
-            acceleration = Acceleration.NONE
-
-        return turn_angle, acceleration
-
-    def advance(self, turn_angle: float, acceleration: Acceleration) -> None:
-        """Advance to the next frame."""
-
-        self.car.move(turn_angle, acceleration)
-        self.track.update_gate(self.car)
-        if not self.track.check_in_bounds(self.car.outline):
-            self.track.place_car_at_start(self.car)
 
     def update_screen(self) -> None:
         """Draw the current frame to the screen."""
@@ -98,17 +44,8 @@ class Game:
         # Show the changes
         pygame.display.flip()
 
-    def run(self) -> None:
-        """Run the main game loop."""
-
-        while True:
-            self.advance(*self.check_move())
-            self.update_screen()
-
-            self.clock.tick(60)
-
 
 if __name__ == '__main__':
     track_save_name = sys.argv[1]
-    game = Game(track_save_name)
+    game = GameVisual(track_save_name)
     game.run()
